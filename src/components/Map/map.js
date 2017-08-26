54a8de89a7ef52b784a75bd173b15a60a984be6c
Michael Gardner <gardnm4@rpi.edu>
2017-08-26 16:53:09 -0400

    map is now displaying

--- src/components/Map/map.js
+++ src/components/Map/map.js
@@ -1,68 +1,73 @@
 import React, {Component} from 'react';
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+
 // import _ from 'lodash';
-import {
-  withGoogleMap,
-  GoogleMap,
-  InfoWindow,
-  Marker,
-  Circle,
-  // MarkerClusterer
-} from 'react-google-maps';
-import {default as MarkerClusterer} from 'react-google-maps/lib/addons/MarkerClusterer';
+// import {
+//   withGoogleMap,
+//   GoogleMap,
+//   InfoWindow,
+//   Marker,
+//   Circle,
+//   // MarkerClusterer
+// } from 'react-google-maps';
+// import {default as MarkerClusterer} from 'react-google-maps/lib/addons/MarkerClusterer';
 
 import s from './map.css';
 
+const Map = ReactMapboxGl({
+accessToken: "pk.eyJ1IjoiZ2FyZG5tNCIsImEiOiJjaXVoaWNkNmIwMHNuMnRsM2s1ZGJoa2JsIn0.J3aEjd8rUNorM-6KwwpJEg"
+});
 // const API_KEY = 'AIzaSyDuUqpv6shuq8CIWgVjLdmVLm8SU8eSHU0';
 
 
 
 
-const GoogleMapContainer = withGoogleMap(props => (
-  <GoogleMap
-    ref={props.onMapLoad}
-    defaultZoom={12}
-    center={props.center}
-    onClick={props.onMapClick}
-  >
-    <MarkerClusterer
-      averageCenter
-      enableRetinaIcons
-      gridSize={60}
-    >
-      {props.markers.map(marker => (
-        <Marker
-          {...marker}
-          onClick = {() => props.onMarkerClick(marker)}
-          onRightClick={() => props.onMarkerRightClick(marker)}
-        >
+// const GoogleMapContainer = withGoogleMap(props => (
+//   <GoogleMap
+//     ref={props.onMapLoad}
+//     defaultZoom={12}
+//     center={props.center}
+//     onClick={props.onMapClick}
+//   >
+//     <MarkerClusterer
+//       averageCenter
+//       enableRetinaIcons
+//       gridSize={60}
+//     >
+//       {props.markers.map(marker => (
+//         <Marker
+//           {...marker}
+//           onClick = {() => props.onMarkerClick(marker)}
+//           onRightClick={() => props.onMarkerRightClick(marker)}
+//         >
+//
+//           {marker.showMsg && (
+//             <InfoWindow onCloseClick={() => props.onMarkerClick(marker)}>
+//               <div>{marker.msg}</div>
+//             </InfoWindow>
+//           )}
+//         </Marker>
+//       ))}
+//     </MarkerClusterer>
+//     {props.center && (
+//       <Circle
+//      center={props.center}
+//      radius={props.center.accr}
+//      options={{
+//        fillColor: `blue`,
+//        fillOpacity: 0.40,
+//        strokeColor: `blue`,
+//        strokeOpacity: 1,
+//        strokeWeight: 1,
+//      }}
+//    />
+//     )}
+//   </GoogleMap>
+// ));
 
-          {marker.showMsg && (
-            <InfoWindow onCloseClick={() => props.onMarkerClick(marker)}>
-              <div>{marker.msg}</div>
-            </InfoWindow>
-          )}
-        </Marker>
-      ))}
-    </MarkerClusterer>
-    {props.center && (
-      <Circle
-     center={props.center}
-     radius={props.center.accr}
-     options={{
-       fillColor: `blue`,
-       fillOpacity: 0.40,
-       strokeColor: `blue`,
-       strokeOpacity: 1,
-       strokeWeight: 1,
-     }}
-   />
-    )}
-  </GoogleMap>
-));
 
 
-
-export default class Map extends Component{
+export default class MapContainer extends Component{
 
   static defaultProps = {
       center: {lat: 40.6976701, lng: -74.2598661},
@@ -74,35 +79,52 @@ export default class Map extends Component{
     this.props = props;
   }
 
-  handleMapLoad = (map) => {
-    this._mapComponent = map;
-    // if (map) {
-    //   console.log(map.getZoom());
-    // }
-  }
+  // handleMapLoad = (map) => {
+  //   this._mapComponent = map;
+  //   // if (map) {
+  //   //   console.log(map.getZoom());
+  //   // }
+  // }
 
-  handleMapClick = (event) => {
-    this.props.onMapClick(event);
-  }
+  // handleMapClick = (event) => {
+  //   this.props.onMapClick(event);
+  // }
+  //
+  // handleMarkerRightClick = (targetMarker) => {
+  //   const nextMarkers = this.props.markers.filter(marker => marker !== targetMarker);
+  //   this.props.updateMarkers(nextMarkers);
+  // }
+  //
+  // handleMarkerClick = (targetMarker) => {
+  //   // console.log("marker clicked",targetMarker)
+  //   if (targetMarker.msg.length === 0) this.handleMarkerRightClick(targetMarker);
+  //   else this.props.onMarkerClick(targetMarker)
+  // }
 
-  handleMarkerRightClick = (targetMarker) => {
-    const nextMarkers = this.props.markers.filter(marker => marker !== targetMarker);
-    this.props.updateMarkers(nextMarkers);
-  }
-
-  handleMarkerClick = (targetMarker) => {
-    // console.log("marker clicked",targetMarker)
-    if (targetMarker.msg.length === 0) this.handleMarkerRightClick(targetMarker);
-    else this.props.onMarkerClick(targetMarker)
+  renderMap = (height) => {
+    return (<Map
+  style="mapbox://styles/mapbox/streets-v9"
+  containerStyle={{
+    height: `${height}px`,
+    width: "100vw"
+  }}
+  >
+    <Layer
+      type="symbol"
+      id="marker"
+      layout={{ "icon-image": "marker-15" }}>
+      <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
+    </Layer>
+</Map>)
   }
 
   render(){
     const {height,center} = {...this.props};
     // console.log("mappos", center)
     return (
-      <div className = {s.map}  id = "map" style = {{height : `${height}px`}}>
+      <div >
 
-        <GoogleMapContainer
+        {/* <GoogleMapContainer
           containerElement={
             <div style={{ height: `100%` }} />
           }
@@ -115,8 +137,8 @@ export default class Map extends Component{
           onMarkerRightClick={this.handleMarkerRightClick}
           onMarkerClick={this.handleMarkerClick}
           center = {center}
-        />
-
+        /> */}
+        {this.renderMap(height)}
 
       </div>
 
